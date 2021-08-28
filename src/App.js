85e67b7d609a87ea47d370cf9aac83ac80034e5a
Muhammad Ismail <yahyaismail@gmail.com>
2021-08-28 14:53:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+import Homepage from "./Components/Homepage/Homepage";
+
+function App() {
+  return (
+    <div className='App'>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
