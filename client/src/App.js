183bf8ecca90967bb14e2a062bba069520751866
Muhammad Ismail <yahyaismail@gmail.com>
2021-11-06 18:05:11 -0400

    Refactor routes to use react lazy + Suspense to optimize for performance

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { GlobalStyle } from "./global.styles";
 import Header from "./Components/Header/Header";
-import Homepage from "./pages/Homepage/Homepage";
-import ShopPage from "./pages/ShopPage/ShopPage";
-import SignInAndUp from "./pages/SignInAndUp/SignInAndUp";
-import Checkout from "./pages/Checkout/Checkout";
+
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
+
+const Homepage = lazy(() => import("./pages/Homepage/Homepage"));
+const ShopPage = lazy(() => import("./pages/ShopPage/ShopPage"));
+const Checkout = lazy(() => import("./pages/Checkout/Checkout"));
+const SignInAndUp = lazy(() => import("./pages/SignInAndUp/SignInAndUp"));
 
 function App() {
   const currentUser = useSelector(selectCurrentUser);
@@ -23,14 +25,17 @@ function App() {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout/' component={Checkout} />
-        <Route
-          exact
-          path='/signin'
-          render={() => (currentUser ? <Redirect to='/' /> : <SignInAndUp />)}
-        />
+        <Suspense fallback={<div>...Loading</div>}>
+          <Route exact path='/' component={Homepage} />
+
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout/' component={Checkout} />
+          <Route
+            exact
+            path='/signin'
+            render={() => (currentUser ? <Redirect to='/' /> : <SignInAndUp />)}
+          />
+        </Suspense>
       </Switch>
     </div>
   );
